b71b5a88de961ff27b9e098f4e6fb80fc1deae20
Mihai <mihai.marandici2008@gmail.com>
2024-06-07 15:20:45 +0300

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿document.getElementById('contact-btn').addEventListener('click', function () {
+    document.getElementById('contact-cta').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('sponsors-btn').addEventListener('click', function () {
+    document.getElementById('sponsors').scrollIntoView({ behavior: 'smooth' });
+});
