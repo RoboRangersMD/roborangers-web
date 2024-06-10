9e3506497086ccfd8c6e5f3d1c56f30158795084
Mihai <mihai.marandici2008@gmail.com>
2024-06-10 23:37:42 +0300

    Clean up

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,1 @@
-﻿document.getElementById('contact-btn').addEventListener('click', function () {
-    document.getElementById('contact-cta').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.getElementById('sponsors-btn').addEventListener('click', function () {
-    document.getElementById('sponsors').scrollIntoView({ behavior: 'smooth' });
-});
+﻿
