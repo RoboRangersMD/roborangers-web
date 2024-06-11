bbee91d5a9db1d6448c7ff1ba12144f7ce631958
Mihai <mihai.marandici2008@gmail.com>
2024-06-11 23:11:18 +0300

    Mobile version

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿document.getElementById('view-more').addEventListener('click', function () {
+    document.getElementById('view-more-target').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('view-team').addEventListener('click', function () {
+    document.getElementById('view-team-target').scrollIntoView({ behavior: 'smooth' });
+});
