189ec431a36ab18cdb253f2e86ec4a496d31f5b5
Mihai <mihai.marandici2008@gmail.com>
2024-06-12 17:42:39 +0300

    Finished desktop version

--- Program.cs
+++ Program.cs
@@ -6,11 +6,8 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+app.UseExceptionHandler("/Error");
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,3 +5,7 @@
 document.getElementById('view-team').addEventListener('click', function () {
     document.getElementById('view-team-target').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.getElementById('contact-button').addEventListener('click', function () {
+    document.getElementById('contact-button-target').scrollIntoView({ behavior: 'smooth' });
+});
